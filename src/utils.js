cd1b2671916f3981a3e08696728bfa3f30fe2834
Semka <iillyyaa907@gmail.com>
2023-02-04 11:47:00 +0700

    first commit

---
+++ src/utils.js
@@ -0,0 +1,8 @@
+export function getRandomColor() {
+  const hexCodes = '0123456789ABCDEF'
+  let color = ''
+  for (let i = 0; i < 6; i++) {
+    color += hexCodes[Math.floor(Math.random() * hexCodes.length)]
+  }
+  return '#' + color
+}
